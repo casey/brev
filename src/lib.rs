bdab0c537288101c53a07ccf2ac40d7952d0eadb
Casey Rodarmor <casey@rodarmor.com>
2016-06-17 15:20:47 -0700

    Use Till's suggestions

--- src/lib.rs
+++ src/lib.rs
@@ -6,31 +6,31 @@ pub fn glob(pattern: &str) -> Vec<String> {
   match glob::glob(pattern) {
     Ok(matches) => matches
       .filter_map(Result::ok)
-      .filter_map(|p| p.as_path().to_str().map(|s| s.to_string()))
+      .filter_map(|p| p.as_path().to_str().map(str::to_owned))
       .collect(),
     Err(err) => panic!("glob: error globbing: {}", err),
   }
 }
 
-pub fn touch(path: &str) {
+pub fn touch<P: AsRef<std::path::Path>>(path: P) {
   if let Err(err) = std::fs::OpenOptions::new().create(true).append(true).open(path) {
     panic!("touch: {}", err)
   }
 }
 
-pub fn mkdir(path: &str) {
+pub fn mkdir<P: AsRef<std::path::Path>>(path: P) {
   if let Err(err) = std::fs::create_dir(path) {
     panic!("mkdir: {}", err)
   }
 }
 
-pub fn rmdir(path: &str) {
+pub fn rmdir<P: AsRef<std::path::Path>>(path: P) {
   if let Err(err) = std::fs::remove_dir(path) {
     panic!("rmdir: {}", err)
   }
 }
 
-pub fn rm(path: &str) {
+pub fn rm<P: AsRef<std::path::Path>>(path: P) {
   if let Err(err) = std::fs::remove_file(path) {
     panic!("rm: {}", err);
   }
