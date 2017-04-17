804a9f5f00a314ed26f6d817b594d3e0195c8adb
Casey Rodarmor <casey@rodarmor.com>
2017-04-16 21:51:20 -0700

    Add home() function to get homedir

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,12 @@ extern crate tempdir;
 use std::io::prelude::*;
 use std::{env, fmt, io, process, path, fs, iter};
 
+pub fn home() -> path::PathBuf {
+  env::home_dir().unwrap_or_else(
+    || panic!("home: failed to get home directory")
+  )
+}
+
 pub fn empty<T, C: iter::FromIterator<T>>() -> C {
   iter::empty().collect()
 }
